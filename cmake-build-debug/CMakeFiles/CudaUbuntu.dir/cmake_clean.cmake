fe8c584631fc7df0f46463a2ec57c76331fd9ecc
ThibLMT <thibaut.lemalet@gmail.com>
2021-03-22 00:17:40 +0100

    Initialized project

---
+++ cmake-build-debug/CMakeFiles/CudaUbuntu.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CudaUbuntu.dir/cmake_device_link.o"
+  "CMakeFiles/CudaUbuntu.dir/main.cu.o"
+  "CudaUbuntu"
+  "CudaUbuntu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/CudaUbuntu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CudaUbuntu.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CudaUbuntu.
+# This may be replaced when dependencies are built.
